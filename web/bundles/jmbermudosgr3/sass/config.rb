b8ce8658e73f4a125abf9d9f6dd8fac25ac7ecc9
Jose <bermudo@gmail.com>
2014-05-03 16:34:40 +0200

    Actualizados los diseños para adaptarse a la imagen final

--- web/bundles/jmbermudosgr3/sass/config.rb
+++ web/bundles/jmbermudosgr3/sass/config.rb
@@ -7,7 +7,8 @@ css_dir = "css"
 css_path = "../css"
 sass_dir = "sass"
 images_dir = "../images"
-javascripts_dir = "../js"
+javascripts_dir = "js"
+javascripts_path = "../css"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
